2dd05db8b38d2b5ef4ac9fe6028c53fb601f46fa
Nilesh Patil <nileshpatil2514@gmail.com>
2022-05-17 11:08:07 +0530

    added pipe, directive and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { MainContainerComponent } from './main-container/main-container.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserComponent } from './user/user.component';
+import { HighlightDirective } from './highlight.directive';
+import { TransferPipe } from './transfer.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    MainContainerComponent,
+    UserListComponent,
+    UserComponent,
+    HighlightDirective,
+    TransferPipe
   ],
   imports: [
     BrowserModule
